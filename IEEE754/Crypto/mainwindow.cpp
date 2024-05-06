a11a54c0d1c314e6c6b5b78c44983b648d426d54
OGUZOK307 <160173934+OGUZOK307@users.noreply.github.com>
2024-05-06 22:50:53 +0300

    Add files via upload

---
+++ IEEE754/Crypto/mainwindow.cpp
@@ -0,0 +1,87 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    if(ui->radioButton_encrypt->isChecked()){
+        ui->pushButton->setText("Зашифровать");
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_radioButton_encrypt_clicked(bool checked)
+{
+    if(checked){
+        ui->pushButton->setText("Зашифровать");
+    }
+}
+
+
+void MainWindow::on_radioButton_decrypt_clicked(bool checked)
+{
+    if(checked){
+        ui->pushButton->setText("Расшифровать");
+    }
+}
+
+
+// Функция для шифрования
+string Encrypt(const string &text, const string &key) {
+    string result;
+    int keyIndex = 0;
+
+    for (char c : text) {
+        if (isalpha(c)) {
+            char base = isupper(c) ? 'A' : 'a';
+            c = (c - base + (toupper(key[keyIndex]) - 'A')) % 26 + base;
+            keyIndex = (keyIndex + 1) % key.length();
+        }
+        result += c;
+    }
+
+    return result;
+}
+
+// Функция для расшифрования
+string Decrypt(const string &text, const string &key) {
+    string result;
+    int keyIndex = 0;
+
+    for (char c : text) {
+        if (isalpha(c)) {
+            char base = isupper(c) ? 'A' : 'a';
+            c = (c - base - (toupper(key[keyIndex]) - 'A') + 26) % 26 + base;
+            keyIndex = (keyIndex + 1) % key.length();
+        }
+        result += c;
+    }
+    return result;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->plainTextEdit_out->clear();
+
+
+    string text = ui->plainTextEdit_in->toPlainText().toStdString();
+    string key = ui->lineEdit_key->text().toStdString();
+
+    if(ui->radioButton_encrypt->isChecked()){
+        text = Encrypt(text, key);
+    }else{
+        text = Decrypt(text, key);
+    }
+
+    QString qstring = QString::fromStdString(text);
+    ui->plainTextEdit_out ->insertPlainText(qstring);
+}
+
